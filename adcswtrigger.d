dbc513452432d3125f7d85ddfcc56e2258159b26
Rebecca Ho <rebeccaho936@gmail.com>
2017-01-31 15:27:50 -0600

    Copied in the ADCSWTrigger_4C123 project.

---
+++ adcswtrigger.d
@@ -0,0 +1,3 @@
+.\adcswtrigger.o: ADCSWTrigger.c
+.\adcswtrigger.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\adcswtrigger.o: ../ValvanoWareTM4C123/ValvanoWareTM4C123/inc/tm4c123gh6pm.h
---
+++ adctestmain.d
@@ -0,0 +1,5 @@
+.\adctestmain.o: ADCTestMain.c
+.\adctestmain.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\adctestmain.o: ADCSWTrigger.h
+.\adctestmain.o: ../ValvanoWareTM4C123/ValvanoWareTM4C123/inc/tm4c123gh6pm.h
+.\adctestmain.o: PLL.h
